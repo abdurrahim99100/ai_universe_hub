5319542a963f6d38aa2ee86d87e39520a7ad4dbb
cornollc <cornollc@gmail.com>
2023-03-04 15:39:29 +0600

    modal problem solv

--- js/ai-hub.js
+++ js/ai-hub.js
@@ -11,7 +11,7 @@ function displayAlldata(data) {
     data = data.slice(0, 6)
 
     for (const user of data) {
-        console.log(user.published_in);
+        // console.log(user.id);
         const mainContainer = document.getElementById('main-container');
 
         const creatElement = document.createElement('div');
@@ -29,7 +29,8 @@ function displayAlldata(data) {
             <h3 class="">${user.name}</h3>
             <div class="d-flex justify-content-between my-5">
                 <p><img src="icon/calendar.png" alt=""> ${user.published_in}</p>
-                <button class="border-0" style="height: 20px; width: 20px;"><img src="icon/add.png" alt=""></button>
+
+                <button onclick="lodeModalData('${user.id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="icon/add.png" alt=""></button>
             </div>
         </div>
     </div>
@@ -38,10 +39,81 @@ function displayAlldata(data) {
     }
 };
 
-// show all button javascript;
-// document.getElementById('btn-showall').addEventListener('click', function(){
+
+
+// phone modal open;
+
+// function lodeModalData() {
 //     const url = (`https://openapi.programming-hero.com/api/ai/tools`);
 //     fetch(url)
-//     .then(res => res.json())
-//     .then(data => displayAlldata(data.data.tools))
-// });
+//         .then(res => res.json())
+//         .then(data => displayModalData(data.data.tools))
+// };
+
+const lodeModalData = async id => {
+    
+    
+    
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.accuracy.score);
+    
+    // discription;
+    const modalDetails = document.getElementById('modal-discription');
+    modalDetails.innerText = data.data.description;
+    
+    // price
+    const priceBasec = document.getElementById('s-basic');
+    priceBasec.innerText = data.data.pricing[0].plan;
+
+    const basicAmount = document.getElementById('b-amount');
+    basicAmount.innerText = data.data.pricing[0].price;
+
+    const pricePro = document.getElementById('s-pro');
+    pricePro.innerText = data.data.pricing[1].plan;
+
+    const proAmount = document.getElementById('p-Amount');
+    proAmount.innerText = data.data.pricing[1].price;
+
+    const priceEh = document.getElementById('prich-eh');
+    priceEh.innerText = data.data.pricing[2].plan;
+
+    const ehAmount = document.getElementById('eh-amount');
+    ehAmount.innerText = data.data.pricing[2].price;
+
+    // featers
+    const featersOn = document.getElementById('features-name1');
+    featersOn.innerText = data.data.features[1].feature_name;
+    
+    const featersTwo = document.getElementById('features-name2');
+    featersTwo.innerText = data.data.features[2].feature_name;
+    
+    const featersThree = document.getElementById('features-name3');
+    featersThree.innerText = data.data.features[3].feature_name;
+    
+    // Integrations;
+    const integOne = document.getElementById('integ-one');
+    integOne.innerText = data.data.integrations[0];
+    
+    const integTwo = document.getElementById('integ-two');
+    integTwo.innerText = data.data.integrations[1];
+    
+    const integThree = document.getElementById('integ-three');
+    integThree.innerText = data.data.integrations[2];
+
+
+
+    // image;
+    const modalImage = document.getElementById('modal-images');
+    modalImage.innerHTML = `
+        <p class="bg-danger rounded px-2 mt-2" style="width: 120px; position: absolute;right: 30px;">accuracy ${data.data.accuracy.score}</p>
+        <img class="rounded mx-auto" style="width: 330px; height: 200px;" src="${data.data.image_link[0]}" alt="">
+        <h4>Hi, how are you doing today?</h4>
+        <p>I'm doing well, thank you for asking. How can I assist you today?</p>
+    `;
+
+        
+
+
+};
